9e7afd2c7c16ab2028cfc3bc58a2c7042039e245
zfy <2721192982@qq.com>
2023-10-04 19:50:35 +0800

    hot-second commint

--- src/main/java/org/zfy/Main.java
+++ src/main/java/org/zfy/Main.java
@@ -6,6 +6,6 @@ public class Main {
         System.out.println("Hello world2!");
         System.out.println("Hello world3");
         System.out.println("Hello world4");
-        System.out.println("Hello world5");
+        System.out.println("Hello world6");
     }
 }
