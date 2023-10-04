ac98df78d416dd9559f5861da949aa9f6100b3eb
zfy <2721192982@qq.com>
2023-10-04 19:47:07 +0800

    hot-first commint

--- src/main/java/org/zfy/Main.java
+++ src/main/java/org/zfy/Main.java
@@ -3,5 +3,9 @@ package org.zfy;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world2!");
+        System.out.println("Hello world3");
+        System.out.println("Hello world4");
+        System.out.println("Hello world5");
     }
 }
